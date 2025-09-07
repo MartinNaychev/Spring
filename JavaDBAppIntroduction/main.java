d2499757f9ec15a693dc761104efb33402241216
Martin Naychev <martin.naychev@gmail.com>
2025-09-07 20:39:32 +0300

    Update main.java

--- JavaDBAppIntroduction/main.java
+++ JavaDBAppIntroduction/main.java
@@ -4,9 +4,10 @@ import java.sql.*;
 import java.util.Properties;
 import java.util.Scanner;
 
-public class main {
+public class Main {
     public static void main(String[] args) throws SQLException {
         Scanner sc = new Scanner(System.in);
+
         System.out.print("Enter username default (root): ");
         String user = sc.nextLine();
         user = user.equals("") ? "root" : user;
